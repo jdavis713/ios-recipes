d42c20241872c7622dffaea446c99e186a6834e1
Jordan Davis <jordan.m.davis92@gmail.com>
2019-05-20 16:15:52 -0400

    Storyboard Layout
    configured UI storyboard and respective view controllers

---
+++ Recipes/Recipes/Model/RecipeController.swift
@@ -0,0 +1,9 @@
+//
+//  RecipeController.swift
+//  Recipes
+//
+//  Created by Jordan Davis on 5/20/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import Foundation
---
+++ Recipes/Recipes/Resources/MainViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Jordan Davis on 5/20/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+    //MARK: -Actions
+
+    @IBAction func textField(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    //MARK: - Properties
+    
+    @IBOutlet var textField: UITextField!
+    
+    
+}
